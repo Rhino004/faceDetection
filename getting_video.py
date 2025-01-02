ec57f775fe944481d8542c2b69697983397dab24
Ryan Tomas <ryantomas85@gmail.com>
2025-01-02 11:50:13 -0600

    making files to test functions

---
+++ getting_video.py
@@ -0,0 +1,16 @@
+import cv2
+vid = cv2.VideoCapture(0)
+
+# Get the default frame width and height
+frame_width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while True:
+    #reads and stores the frame from the video capture
+    rst, frame = vid.read()
+    #dislpay each frame that was capture 
+    cv2.imshow("video", frame)
+    if cv2.waitKey(1) == ord("q"):#waits until the key is pressed
+        break
+vid.release()
+cv2.destroyAllWindows()#closes all windows
