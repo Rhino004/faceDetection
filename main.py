5dbcd6132f0b9857b5e65d78481f4f2a7d22e489
Ryan Tomas <113748661+Rhino004@users.noreply.github.com>
2025-01-14 14:48:23 -0800

    fixing lines and changeing var names

--- main.py
+++ main.py
@@ -32,15 +32,15 @@ def facebox(facedetect):
         SampleboundingBox = int(boundingBox.xmin * iw), int(boundingBox.ymin * ih), int(boundingBox.width * iw), int(boundingBox.height * ih)
         cv2.rectangle(frame,SampleboundingBox, (255, 0,255), 2)
 
-def compareFace(frame):
+def compareFace(vidS):
     """
     This function compares the live feed from the camera and compares it to the imgs
     stored in the encodings
     """
-    smallimg = cv2.resize(frame, (0,0), None, 0.25, 0.25)
-    smallimg = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    faceFrame = face_recognition.face_locations(smallimg)
-    encodeFrame = face_recognition.face_encodings(smallimg, faceFrame)
+    #smallimg = cv2.resize(frame, (0,0), None, 0.25, 0.25)
+    #smallimg = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    faceFrame = face_recognition.face_locations(vidS)
+    encodeFrame = face_recognition.face_encodings(vidS, faceFrame)
     for encodeFace, facelocation in zip(encodeFrame, faceFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
@@ -51,9 +51,10 @@ def compareFace(frame):
         if matches[matchindex]:
             print("known face Detected", peopleIds[matchindex])
 
-vid = vid = cv2.VideoCapture(0)
+vid = cv2.VideoCapture(0)
 frame_width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
 
 pTime = 0
 
@@ -66,14 +67,14 @@ encodeListKnown, peopleIds = GetEncoding()
 
 while True:
     success, frame = vid.read()
-    compareFace(frame)
     vidS = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    compareFace(vidS)
     results = FaceDetection.process(vidS)
     if results.detections:
         facebox(results)
-    #fps, pTime = FrameRate(pTime)
-    #cv2.putText(frame, f'fps: {int(fps)}', (0,30), cv2.FONT_HERSHEY_COMPLEX, 1,(0,255,0), 2)
-    #cv2.putText(input vide, text,position, font, scale of the font, color of the text, thinkness )
+    fps, pTime = FrameRate(pTime)
+    cv2.putText(frame, f'fps: {int(fps)}', (0,30), cv2.FONT_HERSHEY_COMPLEX, 1,(0,255,0), 2)
+    #How cv2.putText works (input video, text,position, font, scale of the font, color of the text, thinkness )
     cv2.imshow('webcam', frame)
     if cv2.waitKey(1) == ord("q"):#waits until the key is pressed
         break
